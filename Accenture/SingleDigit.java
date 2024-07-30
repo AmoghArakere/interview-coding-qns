2287e6010c4ed8beae4a433f14ffbc4d21a5b8b0
Amogh <90240269+AmoghArakere@users.noreply.github.com>
2024-07-30 23:41:36 +0530

    Update SingleDigit.java

--- Accenture/SingleDigit.java
+++ Accenture/SingleDigit.java
@@ -1,1 +1,28 @@
+/*Given a number 𝑁 your task is to make N a single digit number by performing the following operations:
+If N is ODD, make it floor(𝑁/2)
+If N is even, make it floor((𝑁−2)/2)
+If N is already a single digit, print it as it is.*/
+
+import java.util.Scanner;
+
+public class Main {  
+    public static void main(String[] args) {
+        
+    Scanner scanner = new Scanner(System.in);
+    int N = scanner.nextInt();
+    
+    while (N > 9) {
+        if (N % 2 == 1) {
+           
+            N = N / 2;
+        } else {
+            
+            N = (N - 2) / 2;
+        }
+    }
+    
+    System.out.println(N);
+        
+    }
+}
 
